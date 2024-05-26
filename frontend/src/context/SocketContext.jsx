4fe36f36f4fd3d224613565ced24517cf89b5066
shagun.kaza <kaza.shagun@gmail.com>
2024-05-26 13:04:10 -0700

    adding render URL for socket.io

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
   
     useEffect(() => {
       if (authUser && !socket) {
-        const newSocket = io("http://localhost:5000", {
+        const newSocket = io("https://react-chat-app-wjzp.onrender.com/", {
           query: {
             userId: authUser._id,
           },
